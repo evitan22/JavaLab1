bd3fb4d5d4f78f77d9cfb2cdf8ffc623de5012ac
evitan22 <kokravets@gmail.com>
2025-02-27 23:52:51 +0200

    Lab1

--- Main.java
+++ Main.java
@@ -1,32 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Cities[] cities = {
-            new Cities("Kyiv", "troeshina", 8, 94, 1001, 5),
-            new Cities("Lviv", "hmelnickogo", 1, 15, 3464),
-            new Cities("Harkiv", "heroiv", 15, 1035, 6865)
+        Audience[] audiences = {
+            new Audience(200, 250),
+            new Audience(120, 103, 18),
+            new Audience(60, 40, 99)
         };
 
-        for (Cities c : cities) {
-            System.out.println(c.getAddress("notary"));
-        }
-        
-        Cities doneck = new Cities("Doneck", "itjwet", 233);
-        System.out.println(doneck.getAddress("123"));
-        System.out.println(Cities.count);
-        System.out.println(Cities.createTimes("Kyiv"));
-        System.out.println(cities[0].getPeopleCount());
+        System.out.println(audiences[0].bookAnAudience("Prihodko", "High Math", 100));
+        System.out.println(audiences[1].bookAnAudience("Mayer", "Programming"));
+        System.out.println(audiences[2].bookAnAudience("Gavrilenko", "English", 70));
 
-        System.out.println(doneck.getPeopleCount());
-        doneck.deregisterPeople(1);
-        System.out.println(doneck.getPeopleCount());
+        System.out.println(audiences[0].getTypeOfLesson());
+        audiences[0].setTypeOfLesson("exam");
+        System.out.println(audiences[0].getTypeOfLesson());
 
-        var info = "We couldn't verify you mother's maiden name.";
-        var intro = "Here is important information about your account security.";
-
-        var firstName = "Joffrey";
-        var greeting = "Hello";
-
-        // BEGIN (write your solution here)
-        System.out.println(greeting + ", " + firstName + "\n" + intro + "\n" + info);
+        System.out.println(Audience.getCountOfAudiences());
     }
 }
